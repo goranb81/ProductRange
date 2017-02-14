89493669ee6332a8cd89c690e4505b1d7809e08e
goran borojevic <goranb81@gmail.com>
2017-02-14 15:17:08 +0100

    Input Excel phase 1

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -25,4 +25,56 @@ class AdminController extends Controller
         // replace this example code with whatever you need
         return $this->render('admin/admin_base.html.twig');
     }
+
+    /**
+     * @Route("/import_pricelist", name="import_pricelist")
+     */
+    public function importAction(Request $request)
+    {
+        // provide supplierName and supplierID
+        // provide name of excel file
+
+        /*
+        product(externalProductId
+                supplierId,
+                supplierName,
+                productName,
+                price,
+                inputDate,
+                status)
+        */
+        // list of objects that represent existing products in pricelist DB table
+        $listOFExistingProducts = getlistOFExistingProducts($supplierId);
+
+        //list of objects that represent products from pricelist's excel file
+        $listOfExcelProducts = getListOfExcelProducts($excelFile);
+
+        //timestamp - start import
+        $dateOfStart = time();
+
+        //loop throw excel products
+        foreach($productExcel in $listOfExcelProducts){
+
+            // product from excel is not found in DB
+            $p = 'not_found';
+
+            //loop throw existing products list and check Does excel product exists in DB
+            foreach($productDB in $listOFExistingProducts){
+                // compare names becouse product from excel and product from database is linked with name
+                if($productDB.getProductName() == $productExcel.getProductName())then
+                    if($productDB.getStatus = 'tresh') then break;
+                    else{
+                        // execute 'UPDATE PRODUCTRANGE SET STATUS="AKTIVAN" WHERE INTERNALPRODUCTID = $productDB.getiNTERNALpRODUCTid'
+                        // execute 'UPDATE PRODUCTRANGE SET PRICE=$PRODUCTeXCEL.GETpRICE() WHERE INTERNALPRODUCTID = $productDB.getiNTERNALpRODUCTid'
+                // execute 'UPDATE PRODUCTRANGE SET INPUTDATE=TIME() WHERE INTERNALPRODUCTID = $productDB.getiNTERNALpRODUCTid'
+
+                //external product exists in DB
+                p = 'found';
+            }
+            }
+
+        }
+
+        return $this->render('admin/import_pricelist.html.twig');
+    }
 }
